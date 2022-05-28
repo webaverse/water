c316e9fb281c6319a6ec00719658d0cc17482c20
aryasalehi <64514807+aryasalehi@users.noreply.github.com>
2022-05-28 15:20:23 +0430

    changing biomes attribute type

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ import metaversefile from 'metaversefile'
 import * as THREE from 'three'
 import { terrainVertex, terrainFragment } from './shaders/terrainShader.js'
 
-const { useApp, useLoaders, useFrame, useCleanup, usePhysics, useInternals } =
-  metaversefile
+const { useApp, useLoaders, useFrame, useCleanup, usePhysics, useCamera, useInternals } = metaversefile
 
-const baseUrl = import.meta.url.replace(/(\/)[^\/\/]*$/, '$1')
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
 const physicsIds = []
 
 export default (e) => {
   const app = useApp()
-  app.name = 'neon-club'
+  app.name = 'dual-contouring-terrain';
   const physics = usePhysics()
-  const camera = useInternals().camera
+  const camera = useCamera();
   // console.log(camera);
+
+  const textureLoader = new THREE.TextureLoader();
 
   //   color: '#f2b02c',
 
@@ -35,7 +36,7 @@ export default (e) => {
     physics.clearChunkRootDualContouring(origin.x, origin.y, origin.z)
   }
 
-  const addChunk = (origin, y = 0) => {
+  const addChunk = origin => {
     const meshData = physics.createChunkMeshDualContouring(
       origin.x,
       origin.y,
@@ -44,6 +45,7 @@ export default (e) => {
 
     const { positions, normals, indices, biomes, biomesWeights } = meshData
 
+    // if the chunk had no points
     if (!positions || !normals || !indices || !biomes || !biomesWeights) {
       return
     }
@@ -61,21 +63,21 @@ export default (e) => {
 
     // console.log(geometry.attributes.biomes.getX(0));
 
-    const earthTexture = new THREE.TextureLoader().load(
+    const earthTexture = textureLoader.load(
       baseUrl + 'assets/textures/EarthBaseColor1.png'
     )
     earthTexture.wrapS = earthTexture.wrapT = THREE.RepeatWrapping
     earthTexture.encoding = THREE.sRGBEncoding
-    const earthNormal = new THREE.TextureLoader().load(
+    const earthNormal = textureLoader.load(
       baseUrl + 'assets/textures/EarthNormal1.png'
     )
     earthNormal.wrapS = earthNormal.wrapT = THREE.RepeatWrapping
 
-    const grassTexture = new THREE.TextureLoader().load(
+    const grassTexture = textureLoader.load(
       baseUrl + 'assets/textures/GrassBaseColor1.png'
     )
     grassTexture.wrapS = grassTexture.wrapT = THREE.RepeatWrapping
-    const grassNormal = new THREE.TextureLoader().load(
+    const grassNormal = textureLoader.load(
       baseUrl + 'assets/textures/GrassNormal1.png'
     )
     grassNormal.wrapS = grassNormal.wrapT = THREE.RepeatWrapping
@@ -83,7 +85,7 @@ export default (e) => {
     const material = new THREE.ShaderMaterial({
       vertexShader: terrainVertex,
       fragmentShader: terrainFragment,
-      wireframe: true,
+      // wireframe: true,
       vertexColors: true,
       side: THREE.FrontSide,
       uniforms: {
@@ -125,8 +127,8 @@ export default (e) => {
         //   },
         // },
         noiseMap: {
-          value: new THREE.TextureLoader().load(
-            baseUrl + '/assets/texture/noiseMap.png'
+          value: textureLoader.load(
+            baseUrl + 'assets/textures/noiseMap.png'
           ),
         },
         uResolution: {
@@ -138,7 +140,7 @@ export default (e) => {
 
     const mesh = new THREE.Mesh(geometry, material)
 
-    mesh.position.y = y
+    // mesh.position.y = y
 
     app.add(mesh)
 
@@ -148,71 +150,37 @@ export default (e) => {
     clearChunkData(origin)
   }
 
-  const chunkGridNumber = 1
-
-  const min = -((64 * chunkGridNumber) / 2)
-  const max = (64 * chunkGridNumber) / 2
-
   const chunkLocalPosition = new THREE.Vector3()
 
+  const chunkSize = 64;
+  const minC = -1;
+  const maxC = 1;
+  const _forAllChunks = (fn) => {
+    for (let cx = minC; cx <= maxC; cx++) {
+      for (let cz = minC; cz <= maxC; cz++) {
+        for (let cy = minC; cy <= maxC; cy++) {
+          fn(cx, cy, cz);
+        }
+      }
+    }
+  };
   // generate the chunk data
-
-  for (let x = min; x < max; x += 64) {
-    for (let y = min; y < max; y += 64) {
-      for (let z = min; z < max; z += 64) {
-        chunkLocalPosition.set(x + 32, y + 32, z + 32)
-        generateChunk(chunkLocalPosition)
-        console.log(
-          'Generating : ' +
-            chunkLocalPosition.x +
-            ', ' +
-            chunkLocalPosition.y +
-            ', ' +
-            chunkLocalPosition.z
-        )
-      }
-    }
-  }
-
+  _forAllChunks((x, y, z) => {
+    chunkLocalPosition.set(x, y, z).multiplyScalar(chunkSize);
+    generateChunk(chunkLocalPosition);
+  });
   // set the lod (switching lod)
-
-  for (let x = min; x < max; x += 64) {
-    for (let y = min; y < max; y += 64) {
-      for (let z = min; z < max; z += 64) {
-        const lod = Math.abs(Math.max(x, y, z)) / 64
-        chunkLocalPosition.set(x + 32, y + 32, z + 32)
-        setChunkLod(chunkLocalPosition, 1)
-        console.log(
-          'Setting Lod : ' +
-            chunkLocalPosition.x +
-            ', ' +
-            chunkLocalPosition.y +
-            ', ' +
-            chunkLocalPosition.z
-        )
-      }
-    }
-  }
-
+  _forAllChunks((x, y, z) => {
+    chunkLocalPosition.set(x, y, z).multiplyScalar(chunkSize);
+    setChunkLod(chunkLocalPosition, 1);
+  });
   // adding the chunk to the scene
-  for (let x = min; x < max; x += 64) {
-    for (let y = min; y < max; y += 64) {
-      for (let z = min; z < max; z += 64) {
-        chunkLocalPosition.set(x + 32, y + 32, z + 32)
-        addChunk(chunkLocalPosition, -40)
-        console.log(
-          'Adding Chunk : ' +
-            chunkLocalPosition.x +
-            ', ' +
-            chunkLocalPosition.y +
-            ', ' +
-            chunkLocalPosition.z
-        )
-      }
-    }
-  }
+  _forAllChunks((x, y, z) => {
+    chunkLocalPosition.set(x, y, z).multiplyScalar(chunkSize);
+    addChunk(chunkLocalPosition);
+  });
 
-  useFrame(({ timestamp }) => {})
+  // useFrame(({ timestamp }) => {})
 
   useCleanup(() => {
     for (const physicsId of physicsIds) {
