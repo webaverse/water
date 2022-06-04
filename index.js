2ad8c10bee6d6fdc6dfebe1cc1021f56e31aee09
Avaer Kazmer <a@modules.io>
2022-06-04 03:11:07 -0400

    Biomes work

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import metaversefile from 'metaversefile';
 import * as THREE from 'three';
 // import { terrainVertex, terrainFragment } from './shaders/terrainShader.js';
 
-const {useApp, useLocalPlayer, useFrame, useCleanup, usePhysics, useHitManager, useDcWorkerManager, useLodder} = metaversefile;
+const {useApp, useLocalPlayer, useFrame, useCleanup, usePhysics, useLoaders, useDcWorkerManager, useLodder} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
@@ -356,26 +356,16 @@ const biomeSpecs = [
     "Vol_24_2_Snow_Ice_Base_Color"
   ]
 ];
-const biomeDataTexture = (() => {
-  const data = new Uint8Array(256 * 4);
-  for (let i = 0; i < biomeSpecs.length; i++) {
-    const biomeSpec = biomeSpecs[i];
-    const [name, colorHex, textureName] = biomeSpec;
-    localColor.setHex(colorHex);
-    data[i * 4] = localColor.r * 255;
-    data[i * 4 + 1] = localColor.g * 255;
-    data[i * 4 + 2] = localColor.b * 255;
-    data[i * 4 + 3] = 255;
-  }
-  const texture = new THREE.DataTexture(data, 256, 1, THREE.RGBAFormat);
-  texture.minFilter = THREE.LinearFilter;
-  texture.magFilter = THREE.LinearFilter;
-  texture.needsUpdate = true;
-  return texture;
-})();
-
+const mapNames = [
+  'Base_Color',
+  'Height',
+  'Normal',
+  'Roughness',
+  'Ambient_Occlusion',
+];
 const biomesPngTexturePrefix = `/images/stylized-textures/png/`;
 const biomesKtx2TexturePrefix = `https://webaverse.github.io/land-textures/`;
+
 const loadImage = u => new Promise((resolve, reject) => {
   const img = new Image();
   img.onload = () => {
@@ -411,13 +401,6 @@ const bakeBiomesAtlas = async ({
   const textureTileSize = size / texturesPerRow;
   const halfTextureTileSize = textureTileSize / 2;
 
-  const mapNames = [
-    'Base_Color',
-    'Height',
-    'Normal',
-    'Roughness',
-    'Ambient_Occlusion',
-  ];
   for (const mapName of mapNames) {
     const neededTextureNames = neededTexturePrefixes.map(prefix => `${prefix}${mapName}`);
 
@@ -484,11 +467,13 @@ const bakeBiomesAtlas = async ({
   const atlasJsonBlob = new Blob([atlasJsonString], {type: 'application/json'});
   downloadFile(atlasJsonBlob, `megatexture-atlas.json`);
 };
-window.bakeBiomesAtlas = bakeBiomesAtlas;
+// window.bakeBiomesAtlas = bakeBiomesAtlas;
 
 class TerrainMesh extends THREE.Mesh {
   constructor({
     physics,
+    biomeDataTexture,
+    atlasTextures,
   }) {
     const allocator = new dcWorkerManager.constructor.GeometryAllocator([
       {
@@ -597,6 +582,11 @@ class TerrainMesh extends THREE.Mesh {
       onBeforeCompile: (shader) => {
         console.log('on before compile', shader);
 
+        shader.uniforms.uEarthBaseColor = {
+          value: earthTexture,
+          needsUpdate: true,
+        };
+
         shader.vertexShader = shader.vertexShader.replace(`#include <uv_pars_vertex>`, `\
 #ifdef USE_UV
 	#ifdef UVS_VERTEX_ONLY
@@ -611,6 +601,8 @@ attribute ivec4 biomes;
 attribute vec4 biomesWeights;
 uniform sampler2D map;
 varying vec4 vSampleColor;
+varying vec3 vWorldPosition;
+varying vec3 vWorldNormal;
 vec4 sampleBiome(ivec4 biomes, vec4 biomesWeights) {
   vec2 uv0 = vec2((float(biomes.x) + 0.5) / 256.0, 0.5);
   vec4 sampledDiffuseColor0 = texture2D(map, uv0);
@@ -633,20 +625,29 @@ vec4 sampleBiome(ivec4 biomes, vec4 biomesWeights) {
   );
 }
         `);
-        shader.vertexShader = shader.vertexShader.replace(`#include <uv_vertex>`, `\
-#ifdef USE_UV
-	vUv = ( uvTransform * vec3( uv, 1 ) ).xy;
-#endif
+        shader.vertexShader = shader.vertexShader.replace(`#include <worldpos_vertex>`, `\
+// #if defined( USE_ENVMAP ) || defined( DISTANCE ) || defined ( USE_SHADOWMAP ) || defined ( USE_TRANSMISSION )
+  vec4 worldPosition = vec4( transformed, 1.0 );
+  #ifdef USE_INSTANCING
+    worldPosition = instanceMatrix * worldPosition;
+  #endif
+  worldPosition = modelMatrix * worldPosition;
+// #endif
 
+vWorldPosition = worldPosition.xyz;
+vWorldNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
 vSampleColor = sampleBiome(biomes, biomesWeights);
         `);
 
         shader.fragmentShader = shader.fragmentShader.replace(`#include <map_pars_fragment>`, `\
 #ifdef USE_MAP
   uniform sampler2D map;
+  uniform sampler2D uEarthBaseColor;
 #endif
 
 varying vec4 vSampleColor;
+varying vec3 vWorldPosition;
+varying vec3 vWorldNormal;
 vec4 triplanarMap(vec3 position, vec3 normal) {
   const float mapScale = 1.;
 
@@ -658,17 +659,70 @@ vec4 triplanarMap(vec3 position, vec3 normal) {
   vec2 ty = position.zx * mapScale;
   vec2 tz = position.xy * mapScale;
 
-  vec4 cx = texture2D(map, tx) * bf.x;
-  vec4 cy = texture2D(map, ty) * bf.y;
-  vec4 cz = texture2D(map, tz) * bf.z;
+  vec4 cx = texture2D(uEarthBaseColor, tx) * bf.x;
+  vec4 cy = texture2D(uEarthBaseColor, ty) * bf.y;
+  vec4 cz = texture2D(uEarthBaseColor, tz) * bf.z;
 
   vec4 color = cx + cy + cz;
   return color;
+}
+vec4 triplanarTexture(sampler2D inputTexture , float scale , float blendSharpness, vec3 vPosition, vec3 vNormal){
+  vec2 uvX = vPosition.zy * scale;
+  vec2 uvY = vPosition.xz * scale;
+  vec2 uvZ = vPosition.xy * scale;
+  
+  vec4 colX = texture2D(inputTexture , uvX);
+  vec4 colY = texture2D(inputTexture , uvY);
+  vec4 colZ = texture2D(inputTexture , uvZ);
+
+  vec3 blendWeight = pow(abs(vNormal), vec3(blendSharpness));
+  blendWeight /= dot(blendWeight,vec3(1));
+
+  return colX * blendWeight.x + colY * blendWeight.y + colZ * blendWeight.z;
+}
+vec4 triplanarNormal(sampler2D inputTexture , float scale , float blendSharpness, vec3 vPosition, vec3 vNormal) {
+  // Tangent Reconstruction
+  // Triplanar uvs
+  vec2 uvX = vPosition.zy * scale;
+  vec2 uvY = vPosition.xz * scale;
+  vec2 uvZ = vPosition.xy * scale;
+  
+  vec4 colX = texture2D(inputTexture , uvX);
+  vec4 colY = texture2D(inputTexture , uvY);
+  vec4 colZ = texture2D(inputTexture , uvZ);
+  // Tangent space normal maps
+  vec3 tx = colX.xyz * vec3(2,2,2) - vec3(1,1,1);
+  vec3 ty = colY.xyz * vec3(2,2,2) - vec3(1,1,1);
+  vec3 tz = colZ.xyz * vec3(2,2,2) - vec3(1,1,1);
+  vec3 weights = abs(vNormal.xyz);
+  weights = weights / (weights.x + weights.y + weights.z);
+  // Get the sign (-1 or 1) of the surface normal
+  vec3 axis = sign(vNormal);
+  // Construct tangent to world matrices for each axis
+  vec3 tangentX = normalize(cross(vNormal, vec3(0.0, axis.x, 0.0)));
+  vec3 bitangentX = normalize(cross(tangentX, vNormal)) * axis.x;
+  mat3 tbnX = mat3(tangentX, bitangentX, vNormal);
+  vec3 tangentY = normalize(cross(vNormal, vec3(0.0, 0.0, axis.y)));
+  vec3 bitangentY = normalize(cross(tangentY, vNormal)) * axis.y;
+  mat3 tbnY = mat3(tangentY, bitangentY, vNormal);
+  vec3 tangentZ = normalize(cross(vNormal, vec3(0.0, -axis.z, 0.0)));
+  vec3 bitangentZ = normalize(-cross(tangentZ, vNormal)) * axis.z;
+  mat3 tbnZ = mat3(tangentZ, bitangentZ, vNormal);
+  // Apply tangent to world matrix and triblend
+  // Using clamp() because the cross products may be NANs
+  vec3 worldNormal = normalize(
+      clamp(tbnX * tx, -1.0, 1.0) * weights.x +
+      clamp(tbnY * ty, -1.0, 1.0) * weights.y +
+      clamp(tbnZ * tz, -1.0, 1.0) * weights.z
+      );
+  return vec4(worldNormal, 0.0);
 }
         `);
         shader.fragmentShader = shader.fragmentShader.replace(`#include <map_fragment>`, `\
 #ifdef USE_MAP
   vec4 sampledDiffuseColor = vSampleColor;
+  // sampledDiffuseColor *= triplanarTexture(uEarthBaseColor, 1., 1., vWorldPosition, vWorldNormal);
+  sampledDiffuseColor = sampledDiffuseColor * 0.5 + 0.5 * triplanarMap(vWorldPosition, vWorldNormal);
   #ifdef DECODE_VIDEO_TEXTURE
     // inline sRGB decode (TODO: Remove this code when https://crbug.com/1256340 is solved)
     sampledDiffuseColor = vec4( mix( pow( sampledDiffuseColor.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), sampledDiffuseColor.rgb * 0.0773993808, vec3( lessThanEqual( sampledDiffuseColor.rgb, vec3( 0.04045 ) ) ) ), sampledDiffuseColor.w );
@@ -685,6 +739,8 @@ vec4 triplanarMap(vec3 position, vec3 normal) {
     this.physics = physics;
     this.allocator = allocator;
     this.physicsObjects = [];
+
+    // window.terrainMesh = this;
   }
   async addChunk(chunk, {
     signal,
@@ -759,10 +815,16 @@ vec4 triplanarMap(vec3 position, vec3 normal) {
 }
 
 class TerrainChunkGenerator {
-  constructor(parent, physics) {
+  constructor(parent, {
+    physics,
+    biomeDataTexture,
+    atlasTextures,
+  } = {}) {
     // parameters
     this.parent = parent;
     this.physics = physics;
+    this.biomeDataTexture = biomeDataTexture;
+    this.atlasTextures = this.atlasTextures;
 
     // mesh
     this.object = new THREE.Group();
@@ -770,6 +832,8 @@ class TerrainChunkGenerator {
 
     this.terrainMesh = new TerrainMesh({
       physics: this.physics,
+      biomeDataTexture: this.biomeDataTexture,
+      atlasTextures: this.atlasTextures,
     });
     this.object.add(this.terrainMesh);
   }
@@ -857,61 +921,76 @@ class TerrainChunkGenerator {
 export default (e) => {
   const app = useApp();
   const physics = usePhysics();
-  // const hitManager = useHitManager();
   const {LodChunkTracker} = useLodder();
 
   app.name = 'dual-contouring-terrain';
 
-  const generator = new TerrainChunkGenerator(this, physics);
-  const tracker = new LodChunkTracker(generator, {
-    chunkWorldSize,
-    numLods,
-    chunkHeight: chunkWorldSize,
-  });
+  let live = true;
+  let generator = null;
+  let tracker = null;
+  e.waitUntil((async () => {
+    const biomeDataTexture = (() => {
+      const data = new Uint8Array(256 * 4);
+      for (let i = 0; i < biomeSpecs.length; i++) {
+        const biomeSpec = biomeSpecs[i];
+        const [name, colorHex, textureName] = biomeSpec;
+        localColor.setHex(colorHex);
+        data[i * 4] = localColor.r * 255;
+        data[i * 4 + 1] = localColor.g * 255;
+        data[i * 4 + 2] = localColor.b * 255;
+        data[i * 4 + 3] = 255;
+      }
+      const texture = new THREE.DataTexture(data, 256, 1, THREE.RGBAFormat);
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter;
+      texture.needsUpdate = true;
+      return texture;
+    })();
 
-  app.add(generator.object);
-  generator.object.updateMatrixWorld();
+    const {ktx2Loader} = useLoaders();
+    const atlasTextures = await Promise.all(mapNames.map(mapName => new Promise((accept, reject) => {
+      ktx2Loader.load(`${biomesKtx2TexturePrefix}build/8k/${mapName}.ktx2`, accept, function onprogress(e) {}, reject);
+    })));
+    // console.log('got atlas textures', atlasTextures);
+    if (!live) return;
 
-  app.getPhysicsObjects = () => generator.getPhysicsObjects();
+    generator = new TerrainChunkGenerator(this, {
+      physics,
+      biomeDataTexture,
+      atlasTextures,
+    });
+    tracker = new LodChunkTracker(generator, {
+      chunkWorldSize,
+      numLods,
+      chunkHeight: chunkWorldSize,
+    });
+
+    app.add(generator.object);
+    generator.object.updateMatrixWorld();
+  })());
+
+  app.getPhysicsObjects = () => generator ? generator.getPhysicsObjects() : [];
 
   // console.log('got hit tracker', app.hitTracker);
   app.addEventListener('hit', e => {
-    generator.hit(e);
+    generator && generator.hit(e);
   });
 
-  /* let lastHitTime = 0;
-  hitManager.addEventListener('hitattempt', e => {
-    const {type, args} = e.data;
-    if (type === 'sword') {
-      const now = performance.now();
-      const timeDiff = now - lastHitTime;
-      if (timeDiff > 1000) {
-        const {
-          position,
-          quaternion,
-          // hitHalfHeight,
-        } = args;
-        generator.hit(position);
-
-        lastHitTime = now;
-      }
-    }
-  }); */
-
   useFrame(() => {
-    const localPlayer = useLocalPlayer();
-    // localMatrix.compose(localPlayer.position, localPlayer.quaternion, localPlayer.scale)
-    localMatrix.copy(localPlayer.matrixWorld)
-      .premultiply(
-        localMatrix2.copy(app.matrixWorld).invert()
-      )
-      .decompose(localVector, localQuaternion, localVector2)
-    // console.log('got pos', localPlayer.position.toArray().join(','), localVector.toArray().join(','));
-    tracker.update(localVector);
+    if (tracker) {
+      const localPlayer = useLocalPlayer();
+      localMatrix.copy(localPlayer.matrixWorld)
+        .premultiply(
+          localMatrix2.copy(app.matrixWorld).invert()
+        )
+        .decompose(localVector, localQuaternion, localVector2)
+      tracker.update(localVector);
+    }
   });
 
   useCleanup(() => {
-    tracker.destroy();
+    live = false;
+    tracker && tracker.destroy();
   });
 
   return app
